8c00c61e2189f9a331dbc474b10b8f1b5b6d2ff2
SeLiNnnn丶Y <selin.cd@foxmail.com>
2019-05-28 20:14:26 +0800

    update method

--- main/main.js
+++ main/main.js
@@ -1,46 +1,65 @@
+//Method 1
 module.exports = function main(str) {
-  // console.log("Debug Info");
-  // return 'Hello World!';
-
-  var rst = "" //最终结果
-  var arr = str.split("") //拆分字符串
-  var SaveNumbers = [] //暂存获取的单个数字
-  var lcdSaveNumbers = [
-    //lcd数字对应 0-9
-    [["._."], ["|.|"], ["|_|"]],
-    [["..."], ["..|"], ["..|"]],
-    [["._."], ["._|"], ["|_."]],
-    [["._. "], ["._|"], ["._|"]],
-    [["..."], ["|_|"], ["..|"]],
-    [["._."], ["|_."], ["._|"]],
-    [["._."], ["|_."], ["|_|"]],
-    [["._."], ["..|"], ["..|"]],
-    [["._."], ["|_|"], ["|_|"]],
-    [["._."], ["|_|"], ["..|"]]
+  const numbers = [
+    ["._.", "...", "._.", "._.", "...", "._.", "._.", "._.", "._.", "._."],
+    ["|.|", "..|", "._|", "._|", "|_|", "|_.", "|_.", "..|", "|_|", "|_|"],
+    ["|_|", "..|", "|_.", "._|", "..|", "._|", "|_|", "..|", "|_|", "..|"]
   ]
-  // 取出单个数字
-  arr.forEach(item => {
-    SaveNumbers.push(item)
-  })
-  //   console.log(SaveNumbers, "保存单个数字") // ["9","1","0"]
+  const result = [[], [], []]
 
-  //每个数字由3行lcd表示
-  for (let i = 0; i < 3; i++) {
-    //遍历每个数字 把对应行拼在一起
-    for (let j = 0, length = SaveNumbers.length; j < length; j++) {
-      //依次取出数字
-      var tempNumber = SaveNumbers[j]
-      //取出lcd数字
-      var lcdNumber = lcdSaveNumbers[tempNumber]
-      //如果不是最后一个数字 用' '拼接
-      if (j != SaveNumbers.length - 1) {
-        rst = rst + lcdNumber[i] + " "
-      } else {
-        //是最后一个数字 换行
-        rst = rst + lcdNumber[i] + "\n"
-      }
-    }
+  for (let s of str) {
+    result[0].push(numbers[0][s])
+    result[1].push(numbers[1][s])
+    result[2].push(numbers[2][s])
   }
-  //   console.log(rst)
-  return rst
+
+  return result.map(row => row.join(" ") + "\n").join("")
 }
+
+// Method 2
+// module.exports = function main(str) {
+//   // console.log("Debug Info");
+//   // return 'Hello World!';
+
+//   var rst = "" //最终结果
+//   var arr = str.split("") //拆分字符串
+//   var SaveNumbers = [] //暂存获取的单个数字
+//   var lcdSaveNumbers = [
+//     //lcd数字对应 0-9
+//     [[]"._.", ["|.|"], ["|_|"]],
+//     [["..."], ["..|"], ["..|"]],
+//     [["._."], ["._|"], ["|_."]],
+//     [["._. "], ["._|"], ["._|"]],
+//     [["..."], ["|_|"], ["..|"]],
+//     [["._."], ["|_."], ["._|"]],
+//     [["._."], ["|_."], ["|_|"]],
+//     [["._."], ["..|"], ["..|"]],
+//     [["._."], ["|_|"], ["|_|"]],
+//     [["._."], ["|_|"], ["..|"]]
+//   ]
+//   // 取出单个数字
+//   arr.forEach(item => {
+//     SaveNumbers.push(item)
+//   })
+//   //   console.log(SaveNumbers, "保存单个数字") // ["9","1","0"]
+
+//   //每个数字由3行lcd表示
+//   for (let i = 0; i < 3; i++) {
+//     //遍历每个数字 把对应行拼在一起
+//     for (let j = 0, length = SaveNumbers.length; j < length; j++) {
+//       //依次取出数字
+//       var tempNumber = SaveNumbers[j]
+//       //取出lcd数字
+//       var lcdNumber = lcdSaveNumbers[tempNumber]
+//       //如果不是最后一个数字 用' '拼接
+//       if (j != SaveNumbers.length - 1) {
+//         rst = rst + lcdNumber[i] + " "
+//       } else {
+//         //是最后一个数字 换行
+//         rst = rst + lcdNumber[i] + "\n"
+//       }
+//     }
+//   }
+//   //   console.log(rst)
+//   return rst
+// }
